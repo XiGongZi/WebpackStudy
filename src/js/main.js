940b1c66a8b248d86b7fb44e18a7a364494f0906
XiGongZi <1079688386@qq.com>
2019-07-01 21:08:12 +0800

    set bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,73 @@
-import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import '../css/index.less';
+import Vue from './vue-2.4.0';
+import resource from './vue-resource-1.3.4';
 
-console.log("webpack-ser11111000");
-$("#app").click(function(){
-    // alert(`这里是inport22`)
-    console.log(112222222)
+// let teml1 = Vue.
+let vm2 = new Vue({
+    el:"#app2",
+    data:{
+        table:[],
+    },
+    resource,
+    methods:{
+        delThis(val){
+            this.table.some((item,i) => {
+                if(item.id === val){
+                    this.table.splice(i,1);
+                    return true;
+                }
+            });
+        }
+    },
+    created(){
+        console.log(__dirname)
+        console.log(__dirname + "json/table.json")
+        this.$http.post("localhost:3001/json/table.json",{},{ emulateJSON: true }).then(result => {
+            console.log(result)
+        });
+    }
 });
-console.log("ok!!")
+let vm = new Vue({
+    el:"#app",
+    data:{
+        name:'hello world',
+        add1:0,
+        add2:0,
+        result:0,
+        calcSel:"+"
+    },
+    methods:{
+        calc(){
+            let val1 = Number(this.add1);
+            let val2 = Number(this.add2);
+            switch (this.calcSel) {
+                case "+":
+                    this.result = val1 + val2;
+                    break;
+                case "-":
+                    this.result = val1 - val2;
+                    break;
+                case "x":
+                    this.result = val1 * val2;
+                    break;
+                case "/":
+                    this.result = val1 / val2;
+                    break;
+                default:
+                    break;
+            }
+        },
+    },
+    created(){
+        console.log(this.add1)
+    }
+});
+
+// console.log("webpack-ser11111000");
+// $("#app").click(function(){
+//     // alert(`这里是inport22`)
+//     console.log(112222222)
+// });
+// console.log("ok!!")
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const htmlPlugin = require('html-webpack-plugin');
 const webpack = require('webpack')
+// import VueResource from 'vue-resource';
+
+
 module.exports = {
     entry:path.join(__dirname,'./src/js/main.js'),
     output:{
@@ -20,11 +23,19 @@ module.exports = {
             template:path.join(__dirname,'./src/index.html'),
             filename:'index.html',
         }),
+        new webpack.ProvidePlugin({
+            "$":"jquery",
+            "jQuery":"jquery",
+            "jquery":"jquery",
+            "window.jQuery":"jquery"
+        }),
     ],
     module: { // 这个节点，用于配置 所有 第三方模块 加载器 
         rules: [ // 所有第三方模块的 匹配规则
           { test: /\.css$/, use: ['style-loader', 'css-loader'] }, //  配置处理 .css 文件的第三方loader 规则
           { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] }, //配置处理 .less 文件的第三方 loader 规则
+          { test: /\.(jpg|png|gif|bmp|jpeg|ico)$/, use: 'url-loader?limit=7631&name=[hash:8]-[name].[ext]' }, // 处理 图片路径的 loader
+          { test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' }, // 处理 字体文件的 loader 
         ]
       }
 }
